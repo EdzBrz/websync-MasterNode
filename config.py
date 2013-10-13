a8fea6b2caa17be4c33a8b26b9e6aa9a89970136
D7020E <d7020e@eDZ-VirtualBox.(none)>
2013-10-13 18:23:39 +0200

    Moved dynamic run.py to websync-node repo

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-import sys
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-portdb=str(sys.argv[-1])
 DEBUG = False
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, (portdb + '.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
--- run.py
+++ run.py
@@ -1,8 +1,2 @@
-from app import app, db
-
-if __name__ == '__main__':
-   import sys 
-   port=int(sys.argv[-1])
-   if isinstance(port, int):
-      db.create_all()
-      app.run('0.0.0.0', port=port,debug=True)
+from app import app
+app.run('0.0.0.0',debug=True)
