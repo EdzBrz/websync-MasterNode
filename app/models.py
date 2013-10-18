1840899c230470a3089695c3ad64ec2220463625
Edvin Bruun <edz.did.it@gmail.com>
2013-10-18 19:15:47 +0200

    Node list rendered to dashboard

--- app/models.py
+++ app/models.py
@@ -9,8 +9,19 @@ class Node(db.Model):
       return dict(
          id = self.id,
          ipaddr = self.ipaddr,
-         synced = self.synced
+         synced = self.synced,
+         files = str(self.files)
       )
 
    def __repr__(self):
       return '<Node %s>' % (self.ipaddr)
+
+class NodeFile(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   fileid = db.Column(db.Integer)
+   timestamp = db.Column(db.DateTime)
+   node_id = db.Column(db.Integer, db.ForeignKey('node.id'), nullable=False)
+   node = db.relationship('Node', backref='files')
+   
+   def __repr__(self):
+      return 'File %s' % (self.fileid)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request, abort
 from app import app, db
-from app.models import Node
+from app.models import Node, NodeFile
+import datetime
+from pprint import pprint
 
 @app.route('/', methods = ['GET'])
+@app.route('/nodes', methods = ['GET'])
 def all_nodes():
    ns = Node.query.all()
    nsdict = []
@@ -28,6 +31,31 @@ def delete_node(id):
    db.session.delete(n)
    db.session.commit()
    return jsonify ( {'Deleted Node':id} ), 200
+
+@app.route('/post/', methods = ['POST'])
+def add_file():
+   if request.json:
+      nodeURL = 'http://'+(request.remote_addr)+':'+str(request.json['port'])+'/'
+      ts = datetime.datetime.strptime(request.json['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+      n = db.session.query(Node).filter(Node.ipaddr == nodeURL).first()
+      nf = NodeFile(fileid=int(request.json['fileid']), timestamp=ts, node_id=n.id)
+      db.session.add(nf)
+      db.session.commit()
+      return jsonify({'File added': nf.fileid }), 201
+
+# 2013-10-18 14:24:33.399822
+#ns = db.session.query(Node).filter(Node.ipaddr != 'http://'+(request.remote_addr)+':'+str(request.json['port'])+'/'))
+
+@app.route('/put/', methods = ['POST'])
+def update_file():
+   return ""
+
+@app.route('/delete/', methods = ['POST'])
+def delete_file():
+   nodeURL = 'http://'+(request.remote_addr)+':'+str(request.json['port'])+'/'
+   n = db.session.query(Node).filter(Node.ipaddr == nodeURL).first()
+   nf = db.session.query(Nodefile).filter(node_id=='')
+   return ""
 
 # Used for newly created CoreOS VM on OpenStack to get correct docker container
 # Might use dockers own repo system for this though
